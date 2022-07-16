c4f6c6bf980329243dc4c6d3edd476ca7ea3537b
Samuel Speichert <samuelspeichert1607@gmail.com>
2022-07-15 20:39:58 -0400

    feature : main menu added

---
+++ Assets/Scenes/UIScripts/UIScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnClickOptions()
+    {
+
+    }
+
+    public void OnClickCredits()
+    {
+
+    }
+}
