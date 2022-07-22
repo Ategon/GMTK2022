1efb43911c3d18bd8058a39539b222dd9fffdb04
Benjamin Barbeau <benjamin@barbeau.net>
2022-07-22 19:45:32 -0400

    Deal knockback after choosing a powerup

--- Assets/HUD/Health.cs
+++ Assets/HUD/Health.cs
@@ -162,6 +162,8 @@ public class Health : MonoBehaviour
                 Time.timeScale = 0f;
                 powerUpCanvas.GetComponent<PowerUpPool>().FillTextWithPowerUps();
                 level++;
+
+                KnockbackOnDamage();
             }
         }
     }
