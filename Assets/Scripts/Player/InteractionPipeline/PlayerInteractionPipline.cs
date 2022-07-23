2535b4574e16465511eddac31e61cb832a442078
Icecubegame <icecubegame@gmail.com>
2022-07-22 22:29:45 -0400

    Chaged pipeline to store data outside pipeline object

--- Assets/Scripts/Player/InteractionPipeline/PlayerInteractionPipline.cs
+++ Assets/Scripts/Player/InteractionPipeline/PlayerInteractionPipline.cs
@@ -7,7 +7,7 @@ using Cinemachine;
 public class PlayerInteractionPipline : MonoBehaviour
 {
     [SerializeField]
-    PlayerInteractionState initialPlayerState;
+    PlayerInteractionState playerState;
 
     [SerializeField]
     private InteractionPipeline<PlayerInteractionState> pipeline;
@@ -22,31 +22,31 @@ public class PlayerInteractionPipline : MonoBehaviour
     {
         // TOOD (GnoxNahte?): quick fixfor now, might do something different.
         // maybe for some variables just assign directly in inspector
-        initialPlayerState.EntityMovementSettings.RigidBody = GetComponent<Rigidbody>();
+        playerState.EntityMovementSettings.RigidBody = GetComponent<Rigidbody>();
 
-        initialPlayerState.PlayerCameraState.VirtualCamera = GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>();
-        initialPlayerState.PlayerCameraState.CameraFollow = initialPlayerState.PlayerCameraState.VirtualCamera.Follow;
-        initialPlayerState.PlayerCameraState.ScreenSize = new Vector2(Screen.width, Screen.height);
+        playerState.PlayerCameraState.VirtualCamera = GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>();
+        playerState.PlayerCameraState.CameraFollow = playerState.PlayerCameraState.VirtualCamera.Follow;
+        playerState.PlayerCameraState.ScreenSize = new Vector2(Screen.width, Screen.height);
 
-        initialPlayerState.PlayerAttackState.dicePool = new ObjectPool();
-        initialPlayerState.PlayerAttackState.dicePool.InitPool("DicePool", initialPlayerState.PlayerAttackSettings.DicePrefab, 30);
+        playerState.PlayerAttackState.dicePool = new ObjectPool();
+        playerState.PlayerAttackState.dicePool.InitPool("DicePool", playerState.PlayerAttackSettings.DicePrefab, 30);
 
         //initialPlayerState.PlayerAttackState.equippedPowerups = new PowerupSettings[DiceAttackSettings.numOfSides];
         //initialPlayerState.PlayerAttackState.equippedPowerups[0] =
         //    initialPlayerState.PlayerAttackSettings.Powerups[initialPlayerState.PlayerAttackSettings.StartingPowerupIndex];
 
-        foreach (PowerupSettings powerupSetting in initialPlayerState.PlayerAttackState.equippedPowerups)
+        foreach (PowerupSettings powerupSetting in playerState.PlayerAttackState.equippedPowerups)
             powerupSetting.ifEnabled = false;
 
-        initialPlayerState.sharedData.PlayerTransform = transform;
-        initialPlayerState.sharedData.MainCamera = Camera.main;
-        initialPlayerState.sharedData.VirtualCamera = virtualCamera;
-        initialPlayerState.sharedData.GameAudio = gameAudio;
+        playerState.sharedData.PlayerTransform = transform;
+        playerState.sharedData.MainCamera = Camera.main;
+        playerState.sharedData.VirtualCamera = virtualCamera;
+        playerState.sharedData.GameAudio = gameAudio;
     }
 
     public void Start()
     {
-        pipeline = new InteractionPipeline<PlayerInteractionState>(initialPlayerState);
+        pipeline = new InteractionPipeline<PlayerInteractionState>();
 
         InputReader inputReader = GetComponent<InputReader>();
         PlayerStatsModifierGenerator playerAttackPowerupGenerator = GetComponentInChildren<PlayerStatsModifierGenerator>();
@@ -65,8 +65,14 @@ public class PlayerInteractionPipline : MonoBehaviour
         pipeline.AddHandler(playerVisuals.getHandler());
     }
 
+    public void Update()
+    {
+        pipeline.WriteData(ref playerState);
+    }
+
     public void FixedUpdate()
     {
-        pipeline.Execute();
+        playerState.deltaTime = Time.deltaTime;
+        pipeline.HandleData(in playerState);
     }
 }
