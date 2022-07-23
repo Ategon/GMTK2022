edfe19cf86a1a4f4dfda53c912fb7ecedb5d9aaf
Benjamin Barbeau <benjamin@barbeau.net>
2022-07-23 12:56:38 -0400

    Make cursor rotate

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -5,12 +5,23 @@ using UnityEngine.UI;
 
 using DataPipeline;
 
-public class Cursor : MonoBehaviour
+public class Cursor : MonoBehaviour, IHandler<PlayerInteractionState>
 {
-    [SerializeField] private Texture2D cursorTexture;
-
+    Image image;
+    private float rotationSpeed = 100;
+    // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.Cursor.SetCursor(cursorTexture, new Vector2(10, 10), CursorMode.Auto);
+        UnityEngine.Cursor.visible = false;
+        GameObject.Find("Player").GetComponent<PlayerInteractionPipline>().AddHandler(this);
+        image = GetComponent<Image>();
+    }
+
+    public void Handle(in PlayerInteractionState data)
+    {
+        //Vector3 pos = data.sharedData.MainCamera.ScreenToWorldPoint(data.PlayerState.CursorPos);
+        //transform.position = new Vector3(pos.x, 1, pos.z);
+        image.rectTransform.localPosition = new Vector3(data.PlayerState.CursorPos.x - Screen.width/2, data.PlayerState.CursorPos.y - Screen.height / 2, 1);
+        transform.Rotate(0f, 0.0f, rotationSpeed * Time.deltaTime, Space.Self);
     }
 }
